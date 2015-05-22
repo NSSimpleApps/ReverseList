9ca99c85867829c122c0124e9d747de0fa4fc572
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2015-05-22 16:30:41 +0600

    Initial Commit

---
+++ ListReverse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ListReverse
+//
+//  Created by neo on 22.05.15.
+//  Copyright (c) 2015 NSSimpleApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
